4b75015322c62f4beb1fed5c7f8ce6adeb19174f
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-03-29 14:51:04 +0300

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiBasics
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RestApiBasics
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/4");
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<Post>(responseBody);
+
+            Console.WriteLine(responseBody);
+        }
+    }
+}
